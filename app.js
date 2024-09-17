1759a3a113d40aae89a7f026946d1d2465f49711
Astitva1508 <21bcs015@nith.ac.in>
2024-09-17 18:16:59 +0530

    Completed the frontend

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const tasks = require('./routes/tasks');
+const connectDB = require('./db/connect');
+
+const PORT = 3000;
+
+app.use(express.static('./public'));
+app.use(express.json());
+
+app.get('/hello',(req,res)=>{
+    res.send('<h1>Hello World</h1>')
+})
+
+app.use('/api/v1/tasks',tasks);
+
+const start = async()=>{
+    try {
+        await connectDB(process.env.MONGO_URI);
+        app.listen(PORT, () => {
+            console.log(`server is listening on port ${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+start();
+
